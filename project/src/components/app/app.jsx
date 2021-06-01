800cbd7a3f30a00bc3036d96b579027e56878652
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-06-01 23:57:18 +0500

    рефакт components/app

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+import MainPage from '../main-page/main-page';
+import UserPage from '../user-page/user-page';
 
 function App() {
-  return <p>Hello, world!</p>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <MainPage />
+        </Route>
+        <Route path="/mylist">
+          <UserPage />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
