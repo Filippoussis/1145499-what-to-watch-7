b30531fefdb90a9c9cd82277cb68b0f3e6cebb1e
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-06-02 00:05:49 +0500

    создал components/genres-list

---
+++ project/src/components/genres-list/genres-list.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+const GENRES = [
+  'All genres',
+  'Comedies',
+  'Crime',
+  'Documentary',
+  'Dramas',
+  'Horror',
+  'Kids & Family',
+  'Romance',
+  'Sci-Fi',
+  'Thrillers',
+];
+
+function GenresItem({genre}) {
+  return (
+    <li className="catalog__genres-item">
+      <Link to="#" class="catalog__genres-link">{genre}</Link>
+    </li>
+  );
+}
+
+const getGenresItems = () => GENRES.map((genre) => <GenresItem key={genre} genre={genre}/>);
+
+function GenresList() {
+  return (
+    <ul className="catalog__genres-list">
+      {getGenresItems()}
+    </ul>
+  );
+}
+
+GenresItem.propTypes = {
+  genre: PropTypes.string.isRequired,
+};
+
+export default GenresList;
