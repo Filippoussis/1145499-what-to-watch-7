f8b49d4c242647db1268f02c4aea23fa69381aca
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-06-01 23:59:20 +0500

    создал components/main-page

---
+++ project/src/components/main-page/main-page.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import FilmCard from '../film-card/film-card';
+import PageContent from '../page-content/page-content';
+
+function MainPage() {
+  return (
+    <>
+      <FilmCard />
+      <PageContent />
+    </>
+  );
+}
+
+export default MainPage;
