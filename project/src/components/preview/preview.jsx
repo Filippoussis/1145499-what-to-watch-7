2ad462638d58ef97ac0c6240945f07c6864e1d67
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-06-05 01:49:17 +0500

    create preview

---
+++ project/src/components/preview/preview.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Logo from '../page-header/logo/logo';
+import UserBlock from '../page-header/user-block/user-block';
+
+function Preview({film}) {
+
+  const {title, genre, year} = film;
+
+  return (
+    <section className="film-card">
+      <div className="film-card__bg">
+        <img src="img/bg-the-grand-budapest-hotel.jpg" alt="The Grand Budapest Hotel" />
+      </div>
+
+      <h1 className="visually-hidden">WTW</h1>
+
+      <header className="page-header film-card__head">
+        <Logo />
+        <UserBlock />
+      </header>
+
+      <div className="film-card__wrap">
+        <div className="film-card__info">
+          <div className="film-card__poster">
+            <img src="img/the-grand-budapest-hotel-poster.jpg" alt="The Grand Budapest Hotel poster" width="218" height="327" />
+          </div>
+
+          <div className="film-card__desc">
+            <h2 className="film-card__title">{title}</h2>
+            <p className="film-card__meta">
+              <span className="film-card__genre">{genre}</span>
+              <span className="film-card__year">{year}</span>
+            </p>
+
+            <div className="film-card__buttons">
+              <button className="btn btn--play film-card__button" type="button">
+                <svg viewBox="0 0 19 19" width="19" height="19">
+                  <use xlinkHref="#play-s"></use>
+                </svg>
+                <span>Play</span>
+              </button>
+              <button className="btn btn--list film-card__button" type="button">
+                <svg className="0 0 19 20" width="19" height="20">
+                  <use xlinkHref="#add"></use>
+                </svg>
+                <span>My list</span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+Preview.propTypes = {
+  film: PropTypes.shape({
+    title: PropTypes.string,
+    genre: PropTypes.string,
+    year: PropTypes.string,
+  }),
+};
+
+export default Preview;
