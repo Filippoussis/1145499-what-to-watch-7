36acf7ec2e8f609defc00ea6936d84982c99e460
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-06-05 01:38:24 +0500

    renamed component user-page -> my-list

--- project/src/components/my-list/my-list.jsx
+++ project/src/components/my-list/my-list.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 
-import PageHeader from '../page-header/page-header';
+import Logo from '../page-header/logo/logo';
+import UserBlock from '../page-header/user-block/user-block';
 import FilmsList from '../films-list/films-list';
 import PageFooter from '../page-footer/page-footer';
 
 function UserPage() {
   return (
     <div className="user-page">
-      <PageHeader />
+      <header className="page-header user-page__head">
+        <Logo />
+
+        <h1 className="page-title user-page__title">My list</h1>
+
+        <UserBlock />
+      </header>
 
       <section className="catalog">
         <h2 className="catalog__title visually-hidden">Catalog</h2>
