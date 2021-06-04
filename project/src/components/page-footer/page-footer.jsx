42102fef53e5df65678274c67dcbde3de468726d
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-06-05 01:48:13 +0500

    в компоненте page-footer выделил лого в отдельный компонент

--- project/src/components/page-footer/page-footer.jsx
+++ project/src/components/page-footer/page-footer.jsx
@@ -1,16 +1,11 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+
+import Logo from '../page-header/logo/logo';
 
 function PageFooter() {
   return (
     <footer className="page-footer">
-      <div className="logo">
-        <Link class="logo__link logo__link--light">
-          <span className="logo__letter logo__letter--1">W</span>
-          <span className="logo__letter logo__letter--2">T</span>
-          <span className="logo__letter logo__letter--3">W</span>
-        </Link>
-      </div>
+      <Logo footer/>
 
       <div className="copyright">
         <p>© 2019 What to watch Ltd.</p>
