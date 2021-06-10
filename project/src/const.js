0df42e411da27eefe9820dc9b9469948da0699ce
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-06-10 21:37:30 +0500

    завел константу GENRES_COUNT_LIMIT

--- project/src/const.js
+++ project/src/const.js
@@ -1,3 +1,5 @@
 export const STARS_VALUES = ['10', '9', '8', '7', '6', '5', '4', '3', '2', '1'];
 
 export const DEFAULT_GENRE = 'All genres';
+
+export const GENRES_COUNT_LIMIT = 10;
--- project/src/utils.js
+++ project/src/utils.js
@@ -1,11 +1,11 @@
-import {DEFAULT_GENRE} from './const';
+import {DEFAULT_GENRE, GENRES_COUNT_LIMIT} from './const';
 
 const getGenres = (films) => {
   const genres = new Set();
   genres.add(DEFAULT_GENRE);
   films.forEach((film) => genres.add(film.genre));
 
-  return Array.from(genres);
+  return Array.from(genres).slice(0, GENRES_COUNT_LIMIT);
 };
 
 export default getGenres;
