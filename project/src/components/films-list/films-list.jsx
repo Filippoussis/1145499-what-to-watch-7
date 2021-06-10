59c8900093c33aaf5f4ad85845b584586ba4ebf0
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-06-10 23:21:13 +0500

    реализовал переход на страницу Film

--- project/src/components/films-list/films-list.jsx
+++ project/src/components/films-list/films-list.jsx
@@ -1,88 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import SmallFilmCard from './small-film-card/small-film-card';
 
-const FILMS = [
-  {
-    title: 'Fantastic Beasts: The Crimes of Grindelwald',
-    image: 'img/fantastic-beasts-the-crimes-of-grindelwald.jpg',
-  },
-  {
-    title: 'Bohemian Rhapsody',
-    image: 'img/bohemian-rhapsody.jpg',
-  },
-  {
-    title: 'Macbeth',
-    image: 'img/macbeth.jpg',
-  },
-  {
-    title: 'Aviator',
-    image: 'img/aviator.jpg',
-  },
-  {
-    title: 'We need to talk about Kevin',
-    image: 'img/we-need-to-talk-about-kevin.jpg',
-  },
-  {
-    title: 'What We Do in the Shadows',
-    image: 'img/what-we-do-in-the-shadows.jpg',
-  },
-  {
-    title: 'Revenant',
-    image: 'img/revenant.jpg',
-  },
-  {
-    title: 'Johnny English',
-    image: 'img/johnny-english.jpg',
-  },
-  {
-    title: 'Shutter Island',
-    image: 'img/shutter-island.jpg',
-  },
-  {
-    title: 'Pulp Fiction',
-    image: 'img/pulp-fiction.jpg',
-  },
-  {
-    title: 'No Country for Old Men',
-    image: 'img/no-country-for-old-men.jpg',
-  },
-  {
-    title: 'Snatch',
-    image: 'img/snatch.jpg',
-  },
-  {
-    title: 'Moonrise Kingdom',
-    image: 'img/moonrise-kingdom.jpg',
-  },
-  {
-    title: 'Seven Years in Tibet',
-    image: 'img/seven-years-in-tibet.jpg',
-  },
-  {
-    title: 'Midnight Special',
-    image: 'img/midnight-special.jpg',
-  },
-  {
-    title: 'War of the Worlds',
-    image: 'img/war-of-the-worlds.jpg',
-  },
-  {
-    title: 'Dardjeeling Limited',
-    image: 'img/dardjeeling-limited.jpg',
-  },
-  {
-    title: 'Orlando',
-    image: 'img/orlando.jpg',
-  },
-  {
-    title: 'Mindhunter',
-    image: 'img/mindhunter.jpg',
-  },
-];
+import filmProp from '../../props/film';
 
-function FilmsList() {
+function FilmsList({films}) {
 
-  const filmsItems = FILMS.map(({title, image}) => <SmallFilmCard key={title} title={title} image={image}/>);
+  const filmsItems = films.map(({id, name, previewImage}) => <SmallFilmCard key={id} id={id} name={name} previewImage={previewImage}/>);
 
   return (
     <div className="catalog__films-list">
@@ -90,5 +14,9 @@ function FilmsList() {
     </div>
   );
 }
+
+FilmsList.propTypes = {
+  films: PropTypes.arrayOf(filmProp),
+};
 
 export default FilmsList;
--- project/src/components/films-list/small-film-card/small-film-card.jsx
+++ project/src/components/films-list/small-film-card/small-film-card.jsx
@@ -2,22 +2,23 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-function SmallFilmCard({title, image}) {
+function SmallFilmCard({id, name, previewImage}) {
   return (
     <article className="small-film-card catalog__films-card">
       <div className="small-film-card__image">
-        <img src={image} alt={title} width="280" height="175" />
+        <img src={previewImage} alt={name} width="280" height="175" />
       </div>
       <h3 className="small-film-card__title">
-        <Link to="#" className="small-film-card__link" href="film-page.html">{title}</Link>
+        <Link to={`/films/${id}`} className="small-film-card__link">{name}</Link>
       </h3>
     </article>
   );
 }
 
 SmallFilmCard.propTypes = {
-  title: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  previewImage: PropTypes.string.isRequired,
 };
 
 export default SmallFilmCard;
