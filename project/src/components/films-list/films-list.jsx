77e1341e02b0e6c5edcc0758f054926655451f97
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-06-16 21:57:35 +0500

    реализовал активацию видео после 1 секунды

--- project/src/components/films-list/films-list.jsx
+++ project/src/components/films-list/films-list.jsx
@@ -1,38 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import SmallFilmCard from './small-film-card/small-film-card';
 
 import filmProp from '../../props/film';
 
-export default class FilmsList extends Component {
-  constructor() {
-    super();
+function FilmsList({films}) {
 
-    this.state = {
-      activeCardId: '',
-    };
+  const filmsItems = films.map(({id, name, previewImage, previewVideoLink}) =>
+    <SmallFilmCard key={id} id={id} name={name} previewImage={previewImage} previewVideoLink={previewVideoLink} />,
+  );
 
-    this.handleMouseOver = this.handleMouseOver.bind(this);
-  }
-
-  handleMouseOver(id) {
-    this.setState({
-      activeCardId: id,
-    });
-  }
-
-  render() {
-
-    const filmsItems = this.props.films.map(({id, name, previewImage}) => <SmallFilmCard key={id} id={id} name={name} previewImage={previewImage} activateCard={this.handleMouseOver} />);
-
-    return (
-      <div className="catalog__films-list">
-        {filmsItems}
-      </div>
-    );
-  }
+  return (
+    <div className="catalog__films-list">
+      {filmsItems}
+    </div>
+  );
 }
 
 FilmsList.propTypes = {
   films: PropTypes.arrayOf(filmProp),
 };
+
+export default FilmsList;
--- project/src/components/films-list/small-film-card/small-film-card.jsx
+++ project/src/components/films-list/small-film-card/small-film-card.jsx
@@ -1,26 +1,66 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-function SmallFilmCard({id, name, previewImage, activateCard}) {
+export default class SmallFilmCard extends Component {
+  constructor() {
+    super();
 
-  return (
-    <article className="small-film-card catalog__films-card" onMouseOver={() => activateCard(id)}>
+    this.state = {
+      active: false,
+    };
+
+    this.handleMouseEnter = this.handleMouseEnter.bind(this);
+    this.handleMouseLeave = this.handleMouseLeave.bind(this);
+  }
+
+  handleMouseEnter() {
+    this.timer = setTimeout(() => {
+      this.setState({
+        active: true,
+      });
+    }, 1000);
+  }
+
+  handleMouseLeave() {
+    this.setState({
+      active: false,
+    });
+  }
+
+  componentDidUpdate() {
+    clearTimeout(this.timer);
+  }
+
+  render() {
+
+    const {id, name, previewImage, previewVideoLink} = this.props;
+    const {active} = this.state;
+
+    const previewPoster = (
       <div className="small-film-card__image">
         <img src={previewImage} alt={name} width="280" height="175" />
       </div>
-      <h3 className="small-film-card__title">
-        <Link to={`/films/${id}`} className="small-film-card__link">{name}</Link>
-      </h3>
-    </article>
-  );
+    );
+
+    const previewVideo = (
+      <video src={previewVideoLink} width="280" height="175" autoPlay muted></video>
+    );
+
+    return (
+      <article className="small-film-card catalog__films-card" onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave} >
+        {active ? previewVideo : previewPoster}
+        <h3 className="small-film-card__title">
+          <Link to={`/films/${id}`} className="small-film-card__link">{name}</Link>
+        </h3>
+      </article>
+    );
+  }
 }
 
 SmallFilmCard.propTypes = {
   id: PropTypes.number.isRequired,
   name: PropTypes.string.isRequired,
   previewImage: PropTypes.string.isRequired,
-  activateCard: PropTypes.func.isRequired,
+  previewVideoLink: PropTypes.string.isRequired,
 };
-
-export default SmallFilmCard;
