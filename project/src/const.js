bb14e0a717b0fab928125f4618e47945f701a514
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-06-19 21:36:17 +0500

    добавил в const FILM_CARD_NAV_ITEMS

--- project/src/const.js
+++ project/src/const.js
@@ -3,3 +3,5 @@ export const STARS_VALUES = ['10', '9', '8', '7', '6', '5', '4', '3', '2', '1'];
 export const DEFAULT_GENRE = 'All genres';
 
 export const GENRES_COUNT_LIMIT = 10;
+
+export const FILM_CARD_NAV_ITEMS = ['Overview', 'Details', 'Reviews'];
