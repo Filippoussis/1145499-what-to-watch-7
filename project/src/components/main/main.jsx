53c92108d612ad3f33f23cb074a4f926e1a5a9a3
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-06-24 18:22:35 +0500

    рефакт main

--- project/src/components/main/main.jsx
+++ project/src/components/main/main.jsx
@@ -1,5 +1,4 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 
 import Promo from '../promo/promo';
 import GenresList from './genres-list/genres-list';
@@ -7,54 +6,21 @@ import FilmsList from '../films-list/films-list';
 import ShowMore from './show-more/show-more';
 import PageFooter from '../page-footer/page-footer';
 
-import getGenres from '../../utils/genres';
-import filmProp from '../../props/film';
-
-const FILMS_COUNT_STEP = 8;
-
-export default class Main extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      displayedFilmsCount: FILMS_COUNT_STEP,
-    };
-
-    this.handleShowMoreButtonClick = this.handleShowMoreButtonClick.bind(this);
-  }
-
-  handleShowMoreButtonClick() {
-    this.setState((state) => ({
-      displayedFilmsCount: state.displayedFilmsCount + FILMS_COUNT_STEP,
-    }));
-  }
-
-  render() {
-
-    const {films, filmPromo} = this.props;
-    const genres = getGenres(films);
-
-    const {displayedFilmsCount} = this.state;
-    const filmsDisplayed = films.slice(0, Math.min(films.length, displayedFilmsCount));
-
-    return (
-      <>
-        <Promo filmPromo={filmPromo} />
-        <div className="page-content">
-          <section className="catalog">
-            <h2 className="catalog__title visually-hidden">Catalog</h2>
-            <GenresList genres={genres} />
-            <FilmsList films={filmsDisplayed} />
-            {displayedFilmsCount < films.length && <ShowMore incDisplayedFilmsCount={this.handleShowMoreButtonClick} />}
-          </section>
-          <PageFooter />
-        </div>
-      </>
-    );
-  }
+function Main() {
+  return (
+    <>
+      <Promo />
+      <div className="page-content">
+        <section className="catalog">
+          <h2 className="catalog__title visually-hidden">Catalog</h2>
+          <GenresList />
+          <FilmsList />
+          <ShowMore />
+        </section>
+        <PageFooter />
+      </div>
+    </>
+  );
 }
 
-Main.propTypes = {
-  films: PropTypes.arrayOf(filmProp),
-  filmPromo: filmProp,
-};
+export default Main;
