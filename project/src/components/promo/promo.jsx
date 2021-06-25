80756d1308e759760539b2ba3f4b65249ff2d3c9
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-06-25 17:41:19 +0500

    рефакт компонента Promo в связке с redux

--- project/src/components/promo/promo.jsx
+++ project/src/components/promo/promo.jsx
@@ -1,33 +1,26 @@
 import React, {Component} from 'react';
 import {withRouter} from 'react-router-dom';
 import {compose} from 'redux';
+import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 
 import withApiService from '../../hooks/withApiService';
-import apiProp from '../../props/api';
+import {loadPromo} from '../../store/actions/films';
 
+import apiProp from '../../props/api';
+import filmProp from '../../props/film';
 
 import Logo from '../page-header/logo/logo';
 import UserBlock from '../page-header/user-block/user-block';
 
 class Promo extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      promo: {},
-    };
-  }
-
   componentDidMount() {
-    this.setState({
-      promo: this.props.apiService.getPromo(),
-    });
+    const data = this.props.apiService.getPromo();
+    this.props.loadPromo(data);
   }
 
   render() {
-    const {id, name, posterImage, backgroundImage, genre, released} = this.state.promo;
-    const history = this.props.history;
+    const {id, name, posterImage, backgroundImage, genre, released} = this.props.promo;
 
     return (
       <section className="film-card">
@@ -56,7 +49,7 @@ class Promo extends Component {
               </p>
 
               <div className="film-card__buttons">
-                <button className="btn btn--play film-card__button" type="button" onClick={() => history.push(`/player/${id}`)}>
+                <button className="btn btn--play film-card__button" type="button" onClick={() => this.props.history.push(`/player/${id}`)}>
                   <svg viewBox="0 0 19 19" width="19" height="19">
                     <use xlinkHref="#play-s"></use>
                   </svg>
@@ -78,8 +71,18 @@ class Promo extends Component {
 }
 
 Promo.propTypes = {
+  promo: filmProp,
   apiService: apiProp,
   history: PropTypes.object.isRequired,
+  loadPromo: PropTypes.func.isRequired,
 };
 
-export default compose(withApiService, withRouter)(Promo);
+const mapStateToProps = ({promo}) => ({
+  promo,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  loadPromo: (film) => dispatch(loadPromo(film)),
+});
+
+export default compose(withApiService, connect(mapStateToProps, mapDispatchToProps), withRouter)(Promo);
