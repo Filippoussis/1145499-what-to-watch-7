6246f75870561e98a435097831674e382938ac71
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-06-25 13:15:08 +0500

    добавил функционал displayedFilms = 8

--- project/src/components/films-list/films-list.jsx
+++ project/src/components/films-list/films-list.jsx
@@ -5,12 +5,13 @@ import {connect} from 'react-redux';
 
 import withApiService from '../../hooks/withApiService';
 
-import {loadFilms} from '../../actions/films';
+import {loadFilms} from '../../store/actions/films';
 
 import apiProp from '../../props/api';
 import filmProp from '../../props/film';
 
 import SmallFilmCard from './small-film-card/small-film-card';
+import ShowMoreButton from './show-more-button/show-more-button';
 
 class FilmsList extends Component {
   componentDidMount() {
@@ -19,23 +20,33 @@ class FilmsList extends Component {
   }
 
   render() {
-    const filmsItems = this.props.filtredFilms.map((film) => <SmallFilmCard key={film.id} film={film} />);
+
+    const {filtredFilms, displayedFilmsCount} = this.props;
+
+    const filmsItems = filtredFilms.map((film) => <SmallFilmCard key={film.id} film={film} />);
+    const displayedFilms =  filmsItems.slice(0, displayedFilmsCount);
+
     return (
-      <div className="catalog__films-list">
-        {filmsItems}
-      </div>
+      <>
+        <div className="catalog__films-list">
+          {displayedFilms}
+        </div>
+        {displayedFilmsCount <= filmsItems.length ? <ShowMoreButton /> : null}
+      </>
     );
   }
 }
 
 FilmsList.propTypes = {
   filtredFilms: PropTypes.arrayOf(filmProp),
+  displayedFilmsCount: PropTypes.number.isRequired,
   apiService: apiProp,
   loadFilms: PropTypes.func.isRequired,
 };
 
-const mapStateToProps = ({films, currentGenre, defaultGenre}) => ({
+const mapStateToProps = ({films, currentGenre, defaultGenre, displayedFilmsCount}) => ({
   filtredFilms: currentGenre !== defaultGenre ? films.filter((film) => film.genre === currentGenre) : films,
+  displayedFilmsCount,
 });
 
 const mapDispatchToProps = (dispatch) => ({
