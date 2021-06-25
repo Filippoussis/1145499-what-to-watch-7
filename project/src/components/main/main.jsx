0b17e2a76593c2df8a9e345187bfab18900abc0c
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-06-25 13:16:27 +0500

    рефакт main -> перенес ShowBoreButton в FilmsList

--- project/src/components/main/main.jsx
+++ project/src/components/main/main.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import Promo from '../promo/promo';
 import GenresList from './genres-list/genres-list';
 import FilmsList from '../films-list/films-list';
-import ShowMore from './show-more/show-more';
 import PageFooter from '../page-footer/page-footer';
 
 function Main() {
@@ -15,7 +14,6 @@ function Main() {
           <h2 className="catalog__title visually-hidden">Catalog</h2>
           <GenresList />
           <FilmsList />
-          <ShowMore />
         </section>
         <PageFooter />
       </div>
