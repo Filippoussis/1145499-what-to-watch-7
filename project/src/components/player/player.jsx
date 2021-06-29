c3debf794b776a87f3a761119fa9a5cba606031b
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-06-29 11:59:32 +0500

    подключил Player к Redux

--- project/src/components/player/player.jsx
+++ project/src/components/player/player.jsx
@@ -1,54 +1,69 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {withRouter} from 'react-router-dom';
 import {compose} from 'redux';
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 import filmProp from '../../props/film';
 
-function Player({selectedFilm, history}) {
-  return (
-    <div className="player">
-      <video src={selectedFilm.videoLink} className="player__video" poster="img/player-poster.jpg"></video>
+import {ActionCreator} from '../../store/actions/actions';
 
-      <button type="button" className="player__exit" onClick={() => history.goBack()}>Exit</button>
+class Player extends Component {
+  componentDidMount() {
+    const {id} = this.props.match.params;
+    this.props.getActiveFilm(Number(id));
+  }
 
-      <div className="player__controls">
-        <div className="player__controls-row">
-          <div className="player__time">
-            <progress className="player__progress" value="30" max="100"></progress>
-            <div className="player__toggler" style={{left: '30%'}}>Toggler</div>
+  render() {
+    return (
+      <div className="player">
+        <video src={this.props.active.videoLink} className="player__video" poster="img/player-poster.jpg"></video>
+
+        <button type="button" className="player__exit" onClick={() => this.props.history.goBack()}>Exit</button>
+
+        <div className="player__controls">
+          <div className="player__controls-row">
+            <div className="player__time">
+              <progress className="player__progress" value="30" max="100"></progress>
+              <div className="player__toggler" style={{left: '30%'}}>Toggler</div>
+            </div>
+            <div className="player__time-value">1:30:29</div>
           </div>
-          <div className="player__time-value">1:30:29</div>
-        </div>
 
-        <div className="player__controls-row">
-          <button type="button" className="player__play">
-            <svg viewBox="0 0 19 19" width="19" height="19">
-              <use xlinkHref="#play-s"></use>
-            </svg>
-            <span>Play</span>
-          </button>
-          <div className="player__name">Transpotting</div>
+          <div className="player__controls-row">
+            <button type="button" className="player__play">
+              <svg viewBox="0 0 19 19" width="19" height="19">
+                <use xlinkHref="#play-s"></use>
+              </svg>
+              <span>Play</span>
+            </button>
+            <div className="player__name">{this.props.active.name}</div>
 
-          <button type="button" className="player__full-screen">
-            <svg viewBox="0 0 27 27" width="27" height="27">
-              <use xlinkHref="#full-screen"></use>
-            </svg>
-            <span>Full screen</span>
-          </button>
+            <button type="button" className="player__full-screen">
+              <svg viewBox="0 0 27 27" width="27" height="27">
+                <use xlinkHref="#full-screen"></use>
+              </svg>
+              <span>Full screen</span>
+            </button>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 Player.propTypes = {
-  selectedFilm: filmProp,
+  active: filmProp,
+  match: PropTypes.object.isRequired,
   history: PropTypes.object.isRequired,
+  getActiveFilm: PropTypes.func.isRequired,
 };
 
-const mapStateToProps = ({selectedFilm}) => ({
-  selectedFilm,
+const mapStateToProps = ({active}) => ({
+  active,
 });
 
-export default compose(connect(mapStateToProps), withRouter)(Player);
+const mapDispatchToProps = (dispatch) => ({
+  getActiveFilm: (id) => dispatch(ActionCreator.getActiveFilm(id)),
+});
+
+export default compose(connect(mapStateToProps, mapDispatchToProps), withRouter)(Player);
