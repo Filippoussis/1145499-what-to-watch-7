43fdc8d8531b56e8616f0e9b1c49e75ff1e231e1
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-06-29 12:08:18 +0500

    рефакт FilmsList

--- project/src/components/films-list/films-list.jsx
+++ project/src/components/films-list/films-list.jsx
@@ -1,56 +1,22 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import {compose} from 'redux';
-import {connect} from 'react-redux';
-
-import withApiService from '../../hooks/withApiService';
-
-import {loadFilms} from '../../store/actions/films';
-
-import apiProp from '../../props/api';
 import filmProp from '../../props/film';
 
 import SmallFilmCard from './small-film-card/small-film-card';
-import ShowMoreButton from './show-more-button/show-more-button';
 
-class FilmsList extends Component {
-  componentDidMount() {
-    const data = this.props.apiService.getFilms();
-    this.props.loadFilms(data);
-  }
+function FilmsList(props) {
 
-  render() {
+  const filmsItems = props.films.map((film) => <SmallFilmCard key={film.id} film={film} />);
 
-    const {filtredFilms, displayedFilmsCount} = this.props;
-
-    const filmsItems = filtredFilms.map((film) => <SmallFilmCard key={film.id} film={film} />);
-    const displayedFilms =  filmsItems.slice(0, displayedFilmsCount);
-
-    return (
-      <>
-        <div className="catalog__films-list">
-          {displayedFilms}
-        </div>
-        {displayedFilmsCount < filmsItems.length ? <ShowMoreButton /> : null}
-      </>
-    );
-  }
+  return (
+    <div className="catalog__films-list">
+      {filmsItems}
+    </div>
+  );
 }
 
 FilmsList.propTypes = {
-  filtredFilms: PropTypes.arrayOf(filmProp),
-  displayedFilmsCount: PropTypes.number.isRequired,
-  apiService: apiProp,
-  loadFilms: PropTypes.func.isRequired,
+  films: PropTypes.arrayOf(filmProp),
 };
 
-const mapStateToProps = ({films, currentGenre, defaultGenre, displayedFilmsCount}) => ({
-  filtredFilms: currentGenre !== defaultGenre ? films.filter((film) => film.genre === currentGenre) : films,
-  displayedFilmsCount,
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  loadFilms: (films) => dispatch(loadFilms(films)),
-});
-
-export default compose(withApiService, connect(mapStateToProps, mapDispatchToProps))(FilmsList);
+export default FilmsList;
