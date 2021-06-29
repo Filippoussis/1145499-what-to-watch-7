5e9b3fe7602ee4220eb30053f5694fde8eb7da9d
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-06-29 12:08:47 +0500

    рефакт MyList

--- project/src/components/my-list/my-list.jsx
+++ project/src/components/my-list/my-list.jsx
@@ -1,36 +1,22 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 import Logo from '../page-header/logo/logo';
 import UserBlock from '../page-header/user-block/user-block';
-import FilmsList from '../films-list/films-list';
+import Favorites from './favorites/favorites';
 import PageFooter from '../page-footer/page-footer';
 
-import filmProp from '../../props/film';
-
-function MyList({films}) {
+function MyList() {
   return (
     <div className="user-page">
       <header className="page-header user-page__head">
         <Logo />
-
         <h1 className="page-title user-page__title">My list</h1>
-
         <UserBlock />
       </header>
-
-      <section className="catalog">
-        <h2 className="catalog__title visually-hidden">Catalog</h2>
-        <FilmsList films={films} />
-      </section>
-
+      <Favorites />
       <PageFooter />
     </div>
   );
 }
-
-MyList.propTypes = {
-  films: PropTypes.arrayOf(filmProp),
-};
 
 export default MyList;
