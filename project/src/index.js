3b8ea00169ceb8d0961924e2acb30287f46391f8
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-06-29 11:56:06 +0500

    рефакт index.js

--- project/src/index.js
+++ project/src/index.js
@@ -4,23 +4,14 @@ import {BrowserRouter as Router} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import store from './store/store';
 
-import MockApiService from './services/mock-api-service';
-import {ApiServiceProvider} from './components/api-service-context/api-service-context';
-
 import App from './components/app/app';
-
-import FILMS_DATA from './mocks/films';
-
-const apiService = new MockApiService(FILMS_DATA);
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <ApiServiceProvider value={apiService} >
-        <Router>
-          <App />
-        </Router>
-      </ApiServiceProvider>
+      <Router>
+        <App />
+      </Router>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'));
