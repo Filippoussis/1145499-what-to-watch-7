d656315cf0b77fcd8a1655482a57ab69cba10b0c
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-06-29 11:50:43 +0500

    создал actions к глобальному хранилищу

---
+++ project/src/store/actions/actions.js
@@ -0,0 +1,49 @@
+import getGenres from '../../utils/genres';
+
+export const ActionCreator = {
+  loadPromo: (film) => ({
+    type: 'LOAD_PROMO',
+    payload: film,
+  }),
+  loadFilms: (films) => ({
+    type: 'LOAD_FILMS',
+    payload: {
+      films,
+      genres: getGenres(films),
+    },
+  }),
+  loadFilm: (film) => ({
+    type: 'LOAD_FILM',
+    payload: film,
+  }),
+  loadSimilar: (films) => ({
+    type: 'LOAD_SIMILAR',
+    payload: films,
+  }),
+  loadFavorites: (films) => ({
+    type: 'LOAD_FAVORITES',
+    payload: films,
+  }),
+  getActiveFilm: (id) => ({
+    type: 'GET_ACTIVE_FILM',
+    payload: id,
+  }),
+  showMore: () => ({
+    type: 'SHOW_MORE',
+  }),
+  getGenres: (genres) => ({
+    type: 'GET_GENRES',
+    payload: genres,
+  }),
+  chooseGenre: (genre) => ({
+    type: 'CHOOSE_GENRE',
+    payload: genre,
+  }),
+  requireAuthorization: (status) => ({
+    type: 'REQUIRED_AUTHORIZATION',
+    payload: status,
+  }),
+  logout: () => ({
+    type: 'LOGOUT',
+  }),
+};
