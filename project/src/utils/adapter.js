32bc1fa376d6a833b6d3f4b9ec008b4a65a6c1f0
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-06-30 22:28:26 +0500

    убрал лишнее копирование объекта в адаптере

--- project/src/utils/adapter.js
+++ project/src/utils/adapter.js
@@ -1,24 +1,21 @@
 export const adaptFilmsDataToClient = (item) => (
-  Object.assign(
-    {},
-    {
-      id: item.id,
-      name: item.name,
-      posterImage: item.poster_image,
-      previewImage: item.preview_image,
-      backgroundImage: item.background_image,
-      backgroundColor: item.background_color,
-      videoLink: item.video_link,
-      previewVideoLink: item.preview_video_link,
-      description: item.description,
-      rating: item.rating,
-      scoresCount: item.scores_count,
-      director: item.director,
-      starring: item.starring,
-      runTime: item.run_time,
-      genre: item.genre,
-      released: item.released,
-      isFavorite: item.is_favorite,
-    },
-  )
+  {
+    id: item.id,
+    name: item.name,
+    posterImage: item.poster_image,
+    previewImage: item.preview_image,
+    backgroundImage: item.background_image,
+    backgroundColor: item.background_color,
+    videoLink: item.video_link,
+    previewVideoLink: item.preview_video_link,
+    description: item.description,
+    rating: item.rating,
+    scoresCount: item.scores_count,
+    director: item.director,
+    starring: item.starring,
+    runTime: item.run_time,
+    genre: item.genre,
+    released: item.released,
+    isFavorite: item.is_favorite,
+  }
 );
