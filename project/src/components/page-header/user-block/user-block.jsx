b255bcde89635e979926bc6812dd23436e27ed71
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-06-30 20:30:21 +0500

    рефакт компонента user-block - переключние аватарки

--- project/src/components/page-header/user-block/user-block.jsx
+++ project/src/components/page-header/user-block/user-block.jsx
@@ -1,19 +1,49 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {logout} from '../../../store/actions/api-actions';
+import {AuthorizationStatus} from '../../../const';
 
-function UserBlock() {
-  return (
-    <ul className="user-block">
-      <li className="user-block__item">
+function UserBlock(props) {
+
+  const {authorizationStatus, userLogout} = props;
+
+  const notAuth = <div><Link to="/login" className="user-block__link">Sign in</Link></div>;
+  const auth = (
+    <>
+      <div className="user-block__item">
         <div className="user-block__avatar">
           <Link to="/mylist"><img src="img/avatar.jpg" alt="User avatar" width="63" height="63" /></Link>
         </div>
-      </li>
-      <li className="user-block__item">
-        <Link to="#" className="user-block__link">Sign out</Link>
-      </li>
-    </ul>
+      </div>
+      <div className="user-block__item">
+        <span className="user-block__link" onClick={() => userLogout()}>Sign out</span>
+      </div>
+    </>
+  );
+
+  const content = authorizationStatus !== AuthorizationStatus.AUTH ? notAuth : auth;
+
+  return (
+    <div className="user-block">
+      {content}
+    </div>
   );
 }
 
-export default UserBlock;
+UserBlock.propTypes = {
+  userLogout: PropTypes.func.isRequired,
+  authorizationStatus: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = ({authorizationStatus}) => ({
+  authorizationStatus,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  userLogout: () => dispatch(logout()),
+});
+
+export {UserBlock};
+export default connect(mapStateToProps, mapDispatchToProps)(UserBlock);
