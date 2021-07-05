fe804b40f78e5024da896636babb8df35a4e9255
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-07-05 21:52:52 +0500

    добавил в api-actions - fetchComments и sendComment + рефакт then

--- project/src/store/actions/api-actions.js
+++ project/src/store/actions/api-actions.js
@@ -1,30 +1,40 @@
 import {AuthorizationStatus} from '../../const';
 import {ActionCreator} from '../actions/actions';
-import {adaptFilmsDataToClient} from '../../utils/adapter';
+import {adaptFilmDataToClient, adaptCommentClientToServer} from '../../utils/adapter';
 
 export const fetchPromo = () => (dispatch, _getState, api) => (
   api.get('/promo')
-    .then(({data}) => dispatch(ActionCreator.loadPromo(adaptFilmsDataToClient(data))))
+    .then(({data}) => dispatch(ActionCreator.loadPromo(adaptFilmDataToClient(data))))
 );
 
 export const fetchFilms = () => (dispatch, _getState, api) => (
   api.get('/films')
-    .then(({data}) => dispatch(ActionCreator.loadFilms(data.map(adaptFilmsDataToClient))))
+    .then(({data}) => dispatch(ActionCreator.loadFilms(data.map(adaptFilmDataToClient))))
 );
 
 export const fetchFilm = (id) => (dispatch, _getState, api) => (
   api.get(`/films/${id}`)
-    .then(({data}) => dispatch(ActionCreator.loadFilm(adaptFilmsDataToClient(data))))
+    .then(({data}) => dispatch(ActionCreator.loadFilm(adaptFilmDataToClient(data))))
 );
 
 export const fetchSimilar = (id) => (dispatch, _getState, api) => (
   api.get(`/films/${id}/similar`)
-    .then(({data}) => dispatch(ActionCreator.loadSimilar(data.map(adaptFilmsDataToClient))))
+    .then(({data}) => dispatch(ActionCreator.loadSimilar(data.map(adaptFilmDataToClient))))
 );
 
 export const fetchFavorites = () => (dispatch, _getState, api) => (
   api.get('/favorite')
-    .then(({data}) => dispatch(ActionCreator.loadFavorites(data.map(adaptFilmsDataToClient))))
+    .then(({data}) => dispatch(ActionCreator.loadFavorites(data.map(adaptFilmDataToClient))))
+);
+
+export const fetchComments = (filmId) => (dispatch, _getState, api) => (
+  api.get(`/comments/${filmId}`)
+    .then(({data}) => dispatch(ActionCreator.loadComments(data)))
+);
+
+export const sendComment = (filmId, filmData) => (dispatch, _getState, api) => (
+  api.post(`/comments/${filmId}`, adaptCommentClientToServer(filmData))
+    .then(() => dispatch(ActionCreator.redirectToRoute(`/films/${filmId}`)))
 );
 
 export const checkAuth = () => (dispatch, _getState, api) => (
@@ -35,14 +45,18 @@ export const checkAuth = () => (dispatch, _getState, api) => (
 
 export const login = ({login: email, password}) => (dispatch, _getState, api) => (
   api.post('/login', {email, password})
-    .then(({data}) => localStorage.setItem('token', data.token))
-    .then(() => dispatch(ActionCreator.requireAuthorization(AuthorizationStatus.AUTH)))
-    .then(() => dispatch(ActionCreator.redirectToRoute('./')))
+    .then(({data}) => {
+      localStorage.setItem('token', data.token);
+      dispatch(ActionCreator.requireAuthorization(AuthorizationStatus.AUTH));
+      dispatch(ActionCreator.redirectToRoute('/'));
+    })
 );
 
 export const logout = () => (dispatch, _getState, api) => (
   api.delete('/logout')
-    .then(() => localStorage.removeItem('token'))
-    .then(() => dispatch(ActionCreator.logout()))
-    .then(() => dispatch(ActionCreator.redirectToRoute('./')))
+    .then(() => {
+      localStorage.removeItem('token');
+      dispatch(ActionCreator.logout());
+      dispatch(ActionCreator.redirectToRoute('/'));
+    })
 );
