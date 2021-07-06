cdb00d3a4a76cb7210792bea143804264d12be47
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-07-06 22:30:04 +0500

    перевел Promo в функциональный компонент с хуками

--- project/src/components/main/promo/promo.jsx
+++ project/src/components/main/promo/promo.jsx
@@ -1,6 +1,5 @@
-import React, {Component} from 'react';
-import {withRouter} from 'react-router-dom';
-import {compose} from 'redux';
+import React, {useEffect} from 'react';
+import {useHistory} from 'react-router-dom';
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 
@@ -12,65 +11,64 @@ import Logo from '../../page-header/logo/logo';
 import UserBlock from '../../page-header/user-block/user-block';
 import Spinner from '../../spinner/spinner';
 
-class Promo extends Component {
-  componentDidMount() {
-    this.props.fetchPromo();
+function Promo(props) {
+
+  const {id, name, posterImage, backgroundImage, genre, released} = props.film;
+
+  const history = useHistory();
+  const redirect = () => history.push(`/player/${id}`);
+
+  useEffect(() => props.fetchPromo());
+
+  if (!props.loading) {
+    return <Spinner />;
   }
 
-  render() {
+  return (
+    <section className="film-card">
+      <div className="film-card__bg">
+        <img src={backgroundImage} alt={name} />
+      </div>
 
-    if (Object.keys(this.props.promo).length < 1) {
-      return <Spinner />;
-    }
+      <h1 className="visually-hidden">WTW</h1>
 
-    const {id, name, posterImage, backgroundImage, genre, released} = this.props.promo;
+      <header className="page-header film-card__head">
+        <Logo />
+        <UserBlock />
+      </header>
 
-    return (
-      <section className="film-card">
-        <div className="film-card__bg">
-          <img src={backgroundImage} alt={name} />
-        </div>
+      <div className="film-card__wrap">
+        <div className="film-card__info">
+          <div className="film-card__poster">
+            <img src={posterImage} alt={`${name} poster`} width="218" height="327" />
+          </div>
 
-        <h1 className="visually-hidden">WTW</h1>
+          <div className="film-card__desc">
+            <h2 className="film-card__title">{name}</h2>
+            <p className="film-card__meta">
+              <span className="film-card__genre">{genre}</span>
+              <span className="film-card__year">{released}</span>
+            </p>
 
-        <header className="page-header film-card__head">
-          <Logo />
-          <UserBlock />
-        </header>
-
-        <div className="film-card__wrap">
-          <div className="film-card__info">
-            <div className="film-card__poster">
-              <img src={posterImage} alt={`${name} poster`} width="218" height="327" />
-            </div>
-
-            <div className="film-card__desc">
-              <h2 className="film-card__title">{name}</h2>
-              <p className="film-card__meta">
-                <span className="film-card__genre">{genre}</span>
-                <span className="film-card__year">{released}</span>
-              </p>
-
-              <div className="film-card__buttons">
-                <button className="btn btn--play film-card__button" type="button" onClick={() => this.props.history.push(`/player/${id}`)}>
-                  <svg viewBox="0 0 19 19" width="19" height="19">
-                    <use xlinkHref="#play-s"></use>
-                  </svg>
-                  <span>Play</span>
-                </button>
-                <button className="btn btn--list film-card__button" type="button">
-                  <svg className="0 0 19 20" width="19" height="20">
-                    <use xlinkHref="#add"></use>
-                  </svg>
-                  <span>My list</span>
-                </button>
-              </div>
+            <div className="film-card__buttons">
+              <button className="btn btn--play film-card__button" type="button" onClick={redirect}>
+                <svg viewBox="0 0 19 19" width="19" height="19">
+                  <use xlinkHref="#play-s"></use>
+                </svg>
+                <span>Play</span>
+              </button>
+              <button className="btn btn--list film-card__button" type="button">
+                <svg className="0 0 19 20" width="19" height="20">
+                  <use xlinkHref="#add"></use>
+                </svg>
+                <span>My list</span>
+              </button>
             </div>
           </div>
         </div>
-      </section>
-    );
-  }
+      </div>
+    </section>
+  );
 }
 
 Promo.defaultProps = {
@@ -78,17 +76,18 @@ Promo.defaultProps = {
 };
 
 Promo.propTypes = {
-  promo: filmProp,
-  history: PropTypes.object.isRequired,
+  film: filmProp,
+  loading: PropTypes.bool.isRequired,
   fetchPromo: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = ({promo}) => ({
-  promo,
+  film: promo.film,
+  loading: promo.loading,
 });
 
 const mapDispatchToProps = (dispatch) => ({
   fetchPromo: () => dispatch(fetchPromo()),
 });
 
-export default compose(connect(mapStateToProps, mapDispatchToProps), withRouter)(Promo);
+export default connect(mapStateToProps, mapDispatchToProps)(Promo);
