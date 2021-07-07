3658f221a07f758fa73017e2bdb951b539bc7ef9
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-07-07 14:18:55 +0500

    перевел add-review в функциональный с хуками

--- project/src/components/add-review/add-review.jsx
+++ project/src/components/add-review/add-review.jsx
@@ -1,6 +1,5 @@
-import React, {Component} from 'react';
-import {withRouter} from 'react-router-dom';
-import {compose} from 'redux';
+import React, {useEffect} from 'react';
+import {useParams} from 'react-router-dom';
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 import filmProp, {filmDefault} from '../../props/film';
@@ -12,41 +11,40 @@ import Breadcrumbs from '../page-header/breadcrumbs/breadcrumbs';
 import UserBlock from '../page-header/user-block/user-block';
 import ReviewForm from './review-form/review-form';
 
-class AddReview extends Component {
-  componentDidMount() {
-    const {id} = this.props.match.params;
-    this.props.getActiveFilm(Number(id));
-  }
+function AddReview(props) {
 
-  render() {
+  const {active, getActiveFilm} = props;
+  const {id, name, posterImage, backgroundImage} = active;
 
-    const {id, name, posterImage,backgroundImage} = this.props.active;
+  const params = useParams();
+  const {id: matchId} = params;
 
-    return (
-      <section className="film-card film-card--full">
-        <div className="film-card__header">
-          <div className="film-card__bg">
-            <img src={backgroundImage} alt={name} />
-          </div>
+  useEffect(() => getActiveFilm(Number(matchId)));
 
-          <h1 className="visually-hidden">WTW</h1>
-
-          <header className="page-header">
-            <Logo />
-            <Breadcrumbs name={name} />
-            <UserBlock />
-          </header>
-
-          <div className="film-card__poster film-card__poster--small">
-            <img src={posterImage} alt={`${name} poster`} width="218" height="327" />
-          </div>
+  return (
+    <section className="film-card film-card--full">
+      <div className="film-card__header">
+        <div className="film-card__bg">
+          <img src={backgroundImage} alt={name} />
         </div>
 
-        <ReviewForm filmId={id} />
+        <h1 className="visually-hidden">WTW</h1>
 
-      </section>
-    );
-  }
+        <header className="page-header">
+          <Logo />
+          <Breadcrumbs name={name} />
+          <UserBlock />
+        </header>
+
+        <div className="film-card__poster film-card__poster--small">
+          <img src={posterImage} alt={`${name} poster`} width="218" height="327" />
+        </div>
+      </div>
+
+      <ReviewForm filmId={id} />
+
+    </section>
+  );
 }
 
 AddReview.defaultProps = {
@@ -55,7 +53,6 @@ AddReview.defaultProps = {
 
 AddReview.propTypes = {
   active: filmProp,
-  match: PropTypes.object.isRequired,
   getActiveFilm: PropTypes.func.isRequired,
 };
 
@@ -67,4 +64,4 @@ const mapDispatchToProps = (dispatch) => ({
   getActiveFilm: (id) => dispatch(ActionCreator.getActiveFilm(id)),
 });
 
-export default compose(connect(mapStateToProps, mapDispatchToProps), withRouter)(AddReview);
+export default connect(mapStateToProps, mapDispatchToProps)(AddReview);
