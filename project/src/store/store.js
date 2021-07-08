21abfc7cd3eff05a110b403d2e92f73744bda960
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-07-08 22:19:28 +0500

    рефакт store без ActionCreator

--- project/src/store/store.js
+++ project/src/store/store.js
@@ -3,12 +3,12 @@ import thunk from 'redux-thunk';
 import reducer from './reducers/reducer';
 
 import {createAPI} from '../services/api';
-import {ActionCreator} from './actions/actions';
+import {requireAuthorization} from './actions/actions';
 import {AuthorizationStatus} from '../const';
 import {redirect} from './middlewares/redirect';
 
 const api = createAPI(
-  () => store.dispatch(ActionCreator.requireAuthorization(AuthorizationStatus.NO_AUTH)),
+  () => store.dispatch(requireAuthorization(AuthorizationStatus.NO_AUTH)),
 );
 
 const store = createStore(reducer, applyMiddleware(thunk.withExtraArgument(api), redirect));
