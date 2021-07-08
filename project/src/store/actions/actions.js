67a6feb64d10b0330417d004682a64a758df62cb
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-07-08 22:16:23 +0500

    рефакт actions, разделение на отдельные функции

--- project/src/store/actions/actions.js
+++ project/src/store/actions/actions.js
@@ -1,58 +1,57 @@
-import getGenres from '../../utils/genres';
+export const loadPromo = (film) => ({
+  type: 'LOAD_PROMO',
+  payload: film,
+});
 
-export const ActionCreator = {
-  loadPromo: (film) => ({
-    type: 'LOAD_PROMO',
-    payload: film,
-  }),
-  loadFilms: (films) => ({
-    type: 'LOAD_FILMS',
-    payload: {
-      films,
-      genres: getGenres(films),
-    },
-  }),
-  loadFilm: (film) => ({
-    type: 'LOAD_FILM',
-    payload: film,
-  }),
-  loadSimilar: (films) => ({
-    type: 'LOAD_SIMILAR',
-    payload: films,
-  }),
-  loadFavorites: (films) => ({
-    type: 'LOAD_FAVORITES',
-    payload: films,
-  }),
-  getActiveFilm: (id) => ({
-    type: 'GET_ACTIVE_FILM',
-    payload: id,
-  }),
-  loadComments: (comments) => ({
-    type: 'LOAD_COMMENTS',
-    payload: comments,
-  }),
-  showMore: () => ({
-    type: 'SHOW_MORE',
-  }),
-  getGenres: (genres) => ({
-    type: 'GET_GENRES',
-    payload: genres,
-  }),
-  chooseGenre: (genre) => ({
-    type: 'CHOOSE_GENRE',
-    payload: genre,
-  }),
-  requireAuthorization: (status) => ({
-    type: 'REQUIRED_AUTHORIZATION',
-    payload: status,
-  }),
-  logout: () => ({
-    type: 'LOGOUT',
-  }),
+export const loadFilms = (films) => ({
+  type: 'LOAD_FILMS',
+  payload: films,
+});
 
-  redirectToRoute: (url) => ({
-    type: 'REDIRECT_TO_ROUTE',
-    payload: url,
-  }),
-};
+export const loadFilm = (film) => ({
+  type: 'LOAD_FILM',
+  payload: film,
+});
+
+export const loadSimilar = (films) => ({
+  type: 'LOAD_SIMILAR',
+  payload: films,
+});
+
+export const loadFavorites = (films) => ({
+  type: 'LOAD_FAVORITES',
+  payload: films,
+});
+
+export const loadComments = (comments) => ({
+  type: 'LOAD_COMMENTS',
+  payload: comments,
+});
+
+export const setGenre = (genre) => ({
+  type: 'SET_GENRE',
+  payload: genre,
+});
+
+export const showMore = () => ({
+  type: 'SHOW_MORE',
+});
+
+export const getPlayer = (filmId) => ({
+  type: 'GET_PLAYER',
+  payload: filmId,
+});
+
+export const requireAuthorization = (status) => ({
+  type: 'REQUIRED_AUTHORIZATION',
+  payload: status,
+});
+
+export const logout = () => ({
+  type: 'LOGOUT',
+});
+
+export const redirectToRoute = (url) => ({
+  type: 'REDIRECT_TO_ROUTE',
+  payload: url,
+});
--- project/src/store/actions/api-actions.js
+++ project/src/store/actions/api-actions.js
@@ -1,54 +1,51 @@
 import {AuthorizationStatus} from '../../const';
-import {ActionCreator} from '../actions/actions';
 import {adaptFilmDataToClient, adaptCommentClientToServer} from '../../utils/adapter';
+import {
+  loadPromo, loadFilms, loadFilm, loadSimilar, loadFavorites, loadComments,
+  requireAuthorization, redirectToRoute, logout as closeSession
+} from '../actions/actions';
 
 export const fetchPromo = () => (dispatch, _getState, api) => (
   api.get('/promo')
-    .then(({data}) => dispatch(ActionCreator.loadPromo(adaptFilmDataToClient(data))))
+    .then(({data}) => dispatch(loadPromo(adaptFilmDataToClient(data))))
 );
 
 export const fetchFilms = () => (dispatch, _getState, api) => (
   api.get('/films')
-    .then(({data}) => dispatch(ActionCreator.loadFilms(data.map(adaptFilmDataToClient))))
+    .then(({data}) => dispatch(loadFilms(data.map(adaptFilmDataToClient))))
 );
 
 export const fetchFilm = (id) => (dispatch, _getState, api) => (
   api.get(`/films/${id}`)
-    .then(({data}) => dispatch(ActionCreator.loadFilm(adaptFilmDataToClient(data))))
+    .then(({data}) => dispatch(loadFilm(adaptFilmDataToClient(data))))
 );
 
 export const fetchSimilar = (id) => (dispatch, _getState, api) => (
   api.get(`/films/${id}/similar`)
-    .then(({data}) => dispatch(ActionCreator.loadSimilar(data.map(adaptFilmDataToClient))))
+    .then(({data}) => dispatch(loadSimilar(data.map(adaptFilmDataToClient))))
 );
 
 export const fetchFavorites = () => (dispatch, _getState, api) => (
   api.get('/favorite')
-    .then(({data}) => dispatch(ActionCreator.loadFavorites(data.map(adaptFilmDataToClient))))
+    .then(({data}) => dispatch(loadFavorites(data.map(adaptFilmDataToClient))))
 );
 
 export const fetchComments = (filmId) => (dispatch, _getState, api) => (
   api.get(`/comments/${filmId}`)
-    .then(({data}) => dispatch(ActionCreator.loadComments(data)))
+    .then(({data}) => dispatch(loadComments(data)))
 );
 
-export const sendComment = (filmId, filmData) => (dispatch, _getState, api) => (
+export const fetchComment = (filmId, filmData) => (dispatch, _getState, api) => (
   api.post(`/comments/${filmId}`, adaptCommentClientToServer(filmData))
-    .then(() => dispatch(ActionCreator.redirectToRoute(`/films/${filmId}`)))
-);
-
-export const checkAuth = () => (dispatch, _getState, api) => (
-  api.get('/login')
-    .then(() => dispatch(ActionCreator.requireAuthorization(AuthorizationStatus.AUTH)))
-    .catch(() => {})
+    .then(() => dispatch(redirectToRoute(`/films/${filmId}`)))
 );
 
 export const login = (authData) => (dispatch, _getState, api) => (
   api.post('/login', authData)
     .then(({data}) => {
       localStorage.setItem('token', data.token);
-      dispatch(ActionCreator.requireAuthorization(AuthorizationStatus.AUTH));
-      dispatch(ActionCreator.redirectToRoute('/'));
+      dispatch(requireAuthorization(AuthorizationStatus.AUTH));
+      dispatch(redirectToRoute('/'));
     })
 );
 
@@ -56,7 +53,7 @@ export const logout = () => (dispatch, _getState, api) => (
   api.delete('/logout')
     .then(() => {
       localStorage.removeItem('token');
-      dispatch(ActionCreator.logout());
-      dispatch(ActionCreator.redirectToRoute('/'));
+      dispatch(closeSession());
+      dispatch(redirectToRoute('/'));
     })
 );
