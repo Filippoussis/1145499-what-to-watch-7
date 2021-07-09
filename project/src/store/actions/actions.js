049191d138ebcae6af25d20c68bb4da4976fb524
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-07-09 10:20:40 +0500

    рефакт actions по RTK

--- project/src/store/actions/actions.js
+++ project/src/store/actions/actions.js
@@ -1,57 +1,60 @@
-export const loadPromo = (film) => ({
-  type: 'LOAD_PROMO',
+import {createAction} from '@reduxjs/toolkit';
+
+export const ActionType = {
+  LOAD_PROMO: 'LOAD_PROMO',
+  LOAD_FILMS: 'LOAD_FILMS',
+  LOAD_FILM: 'LOAD_FILM',
+  LOAD_SIMILAR: 'LOAD_SIMILAR',
+  LOAD_FAVORITES: 'LOAD_FAVORITES',
+  LOAD_COMMENTS: 'LOAD_COMMENTS',
+  LOAD_PLAYER: 'LOAD_PLAYER',
+  SET_GENRE: 'SET_GENRE',
+  SHOW_MORE: 'SHOW_MORE',
+  LOGOUT: 'LOGOUT',
+  REQUIRED_AUTHORIZATION: 'REQUIRED_AUTHORIZATION',
+  REDIRECT_TO_ROUTE: 'REDIRECT_TO_ROUTE',
+};
+
+export const loadPromo = createAction(ActionType.LOAD_PROMO, (promo) => ({
+  payload: promo,
+}));
+
+export const loadFilms = createAction(ActionType.LOAD_FILMS, (films) => ({
+  payload: films,
+}));
+
+export const loadFilm = createAction(ActionType.LOAD_FILM, (film) => ({
   payload: film,
-});
+}));
 
-export const loadFilms = (films) => ({
-  type: 'LOAD_FILMS',
+export const loadSimilar = createAction(ActionType.LOAD_SIMILAR, (films) => ({
   payload: films,
-});
+}));
 
-export const loadFilm = (film) => ({
-  type: 'LOAD_FILM',
-  payload: film,
-});
-
-export const loadSimilar = (films) => ({
-  type: 'LOAD_SIMILAR',
+export const loadFavorites = createAction(ActionType.LOAD_FAVORITES, (films) => ({
   payload: films,
-});
+}));
 
-export const loadFavorites = (films) => ({
-  type: 'LOAD_FAVORITES',
-  payload: films,
-});
-
-export const loadComments = (comments) => ({
-  type: 'LOAD_COMMENTS',
+export const loadComments = createAction(ActionType.LOAD_COMMENTS, (comments) => ({
   payload: comments,
-});
+}));
 
-export const setGenre = (genre) => ({
-  type: 'SET_GENRE',
+export const setGenre = createAction(ActionType.SET_GENRE, (genre) => ({
   payload: genre,
-});
+}));
 
-export const showMore = () => ({
-  type: 'SHOW_MORE',
-});
+export const showMore = createAction(ActionType.SHOW_MORE);
 
-export const loadPlayer = (filmId) => ({
-  type: 'LOAD_PLAYER',
+export const loadPlayer = createAction(ActionType.LOAD_PLAYER, (filmId) => ({
   payload: filmId,
-});
+}));
 
-export const requireAuthorization = (status) => ({
-  type: 'REQUIRED_AUTHORIZATION',
+export const requireAuthorization = createAction(ActionType.REQUIRED_AUTHORIZATION, (status) => ({
   payload: status,
-});
+}));
 
-export const logout = () => ({
-  type: 'LOGOUT',
-});
+export const logout = createAction(ActionType.LOGOUT);
 
-export const redirectToRoute = (url) => ({
-  type: 'REDIRECT_TO_ROUTE',
+export const redirectToRoute = createAction(ActionType.REDIRECT_TO_ROUTE, (url) => ({
   payload: url,
-});
+}));
