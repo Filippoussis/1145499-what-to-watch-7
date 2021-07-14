5d0c29691a417df812badca24730d62ff2ef52e7
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-07-14 08:56:05 +0500

    добавил в utils-time -> getHumanisedPlayerTime + небольшой рефакт getFormattedTime

--- project/src/utils/time.js
+++ project/src/utils/time.js
@@ -1,10 +1,30 @@
-const MINUTES_PER_HOUR = 60;
-
-export const getFormattedTime = (time) => {
-  const hours = Math.trunc(time / MINUTES_PER_HOUR);
+export const getFormattedTime = (timeInMinutes) => {
+  const hours = Math.trunc(timeInMinutes / 60);
 
   const humanisedHours = hours > 0 ? `${hours}h` : '';
-  const humanisedMinutes = `${time - hours * MINUTES_PER_HOUR}m`;
+  const humanisedMinutes = `${timeInMinutes - hours * 60}m`;
 
   return `${humanisedHours} ${humanisedMinutes}`;
+};
+
+export const getHumanisedPlayerTime = (timeInSeconds) => {
+  if (timeInSeconds === null) {
+    return;
+  }
+
+  const timestamp = Math.floor(timeInSeconds);
+
+  const hours = Math.floor(timestamp / 60 / 60);
+  const minutes = Math.floor(timestamp / 60) - (hours * 60);
+  const seconds = timestamp % 60;
+
+  const humanisedHours = hours.toString().padStart(2, '0');
+  const humanisedMinutes = minutes.toString().padStart(2, '0');
+  const humanisedSeconds = seconds.toString().padStart(2, '0');
+
+  if (hours < 1) {
+    return [humanisedMinutes, humanisedSeconds].join(':');
+  }
+
+  return [humanisedHours, humanisedMinutes, humanisedSeconds].join(':');
 };
