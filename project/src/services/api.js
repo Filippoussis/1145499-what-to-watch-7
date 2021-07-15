56990f857372b72be938bead0118e3aacc86a1c6
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-07-15 21:10:16 +0500

    рефакт api в части токена

--- project/src/services/api.js
+++ project/src/services/api.js
@@ -7,15 +7,10 @@ const HttpCode = {
   UNAUTHORIZED: 401,
 };
 
-const token = localStorage.getItem('token') ?? '';
-
 export const createAPI = (onUnauthorized) => {
   const api = axios.create({
     baseURL: BACKEND_URL,
     timeout: REQUEST_TIMEOUT,
-    headers: {
-      'x-token': token,
-    },
   });
 
   const onSuccess = (response) => response;
@@ -29,6 +24,14 @@ export const createAPI = (onUnauthorized) => {
 
     throw err;
   };
+
+  api.interceptors.request.use((config) => {
+    const token = localStorage.getItem('token') ?? '';
+    if (token) {
+      config.headers['x-token'] = token;
+    }
+    return config;
+  });
 
   api.interceptors.response.use(onSuccess, onFail);
 
