cf051d043ef3ce2b1756a4eb3364aa2f29ad4990
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-07-15 21:12:18 +0500

    добавил проверку статуса авторизации пользователя в index

--- project/src/index.js
+++ project/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import {Router as BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import store from './store/store';
-
 import browserHistory from './browser-history';
 
+import {checkAuth} from './store/actions/api-actions';
+
 import App from './components/app/app';
+
+store.dispatch(checkAuth());
 
 ReactDOM.render(
   <React.StrictMode>
