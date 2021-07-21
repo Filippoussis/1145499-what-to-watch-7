f5f3dc95e5e9a742b49fda0eda5aedf8bf19a057
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-07-21 19:17:44 +0500

    рефакт films-data.test

--- project/src/store/reducers/films-data/films-data.test.js
+++ project/src/store/reducers/films-data/films-data.test.js
@@ -1,9 +1,5 @@
 import {filmsData} from './films-data';
-
-import {
-  loadPromo, loadFilms, loadFilm,
-  loadSimilar, loadFavorites, loadComments
-} from '../../actions/actions';
+import {ActionType} from '../../actions/actions';
 
 const state = {
   promo: {data: {}, loaded: false},
@@ -15,6 +11,90 @@ const state = {
   player: {},
 };
 
+const film = {
+  id: 1,
+  name: 'The Grand Budapest Hotel',
+  posteriImage: 'img/the-grand-budapest-hotel-poster.jpg',
+  previewImage: 'img/the-grand-budapest-hotel.jpg',
+  backgroundImage: 'img/the-grand-budapest-hotel-bg.jpg',
+  backgroundColor: '#ffffff',
+  videoLink: 'https://some-link',
+  previewVideoLink: 'https://some-link',
+  description: 'In the 1930s, the Grand Budapest Hotel is a popular European ski resort...',
+  rating: 8.9,
+  scoresCount: 240,
+  director: 'Wes Andreson',
+  starring: ['Bill Murray', 'Edward Norton', 'Jude Law', 'Willem Dafoe', 'Saoirse Ronan'],
+  runTime: 99,
+  genre: 'Comedy',
+  released: 2014,
+  isFavorite: false,
+};
+
+const films = [
+  {
+    id: 1,
+    name: 'The Grand Budapest Hotel',
+    posteriImage: 'img/the-grand-budapest-hotel-poster.jpg',
+    previewImage: 'img/the-grand-budapest-hotel.jpg',
+    backgroundImage: 'img/the-grand-budapest-hotel-bg.jpg',
+    backgroundColor: '#ffffff',
+    videoLink: 'https://some-link',
+    previewVideoLink: 'https://some-link',
+    description: 'In the 1930s, the Grand Budapest Hotel is a popular European ski resort...',
+    rating: 8.9,
+    scoresCount: 240,
+    director: 'Wes Andreson',
+    starring: ['Bill Murray', 'Edward Norton', 'Jude Law', 'Willem Dafoe', 'Saoirse Ronan'],
+    runTime: 99,
+    genre: 'Comedy',
+    released: 2014,
+    isFavorite: false,
+  },
+  {
+    id: 2,
+    name: 'The Grand Budapest Hotel',
+    posteriImage: 'img/the-grand-budapest-hotel-poster.jpg',
+    previewImage: 'img/the-grand-budapest-hotel.jpg',
+    backgroundImage: 'img/the-grand-budapest-hotel-bg.jpg',
+    backgroundColor: '#ffffff',
+    videoLink: 'https://some-link',
+    previewVideoLink: 'https://some-link',
+    description: 'In the 1930s, the Grand Budapest Hotel is a popular European ski resort...',
+    rating: 8.9,
+    scoresCount: 240,
+    director: 'Wes Andreson',
+    starring: ['Bill Murray', 'Edward Norton', 'Jude Law', 'Willem Dafoe', 'Saoirse Ronan'],
+    runTime: 99,
+    genre: 'Comedy',
+    released: 2014,
+    isFavorite: false,
+  },
+];
+
+const comments = [
+  {
+    id: 1,
+    user: {
+      id: 4,
+      name: 'Kate Muir',
+    },
+    rating: 8.9,
+    comment: 'Discerning travellers and Wes Anderson...',
+    date: '2019-05-08T14:13:56.569Z',
+  },
+  {
+    id: 2,
+    user: {
+      id: 5,
+      name: 'John Golt',
+    },
+    rating: 8.9,
+    comment: 'Discerning travellers and Wes Anderson...',
+    date: '2019-05-08T14:13:56.569Z',
+  },
+];
+
 describe('Reducer: filmsData', () => {
   it('without additional parameters should return initial state', () => {
     expect(filmsData(undefined, {}))
@@ -22,31 +102,25 @@ describe('Reducer: filmsData', () => {
   });
 
   it('loadPromo', () => {
-    const promo = {
-      id: 1,
-      name: 'Kolobok',
+    const loadPromoAction = {
+      type: ActionType.LOAD_PROMO,
+      payload: film,
     };
 
-    expect(filmsData(state, loadPromo(promo)))
+    expect(filmsData(state, loadPromoAction))
       .toEqual({
         ...state,
-        promo: {data: promo, loaded: true},
+        promo: {data: film, loaded: true},
       });
   });
 
   it('loadFilms', () => {
-    const films = [
-      {
-        id: 1,
-        name: 'Kolobok',
-      },
-      {
-        id: 2,
-        name: 'Nu pogodi',
-      },
-    ];
+    const loadFilmsAction = {
+      type: ActionType.LOAD_FILMS,
+      payload: films,
+    };
 
-    expect(filmsData(state, loadFilms(films)))
+    expect(filmsData(state, loadFilmsAction))
       .toEqual({
         ...state,
         films: {data: films, loaded: true},
@@ -54,12 +128,12 @@ describe('Reducer: filmsData', () => {
   });
 
   it('loadFilm', () => {
-    const film = {
-      id: 1,
-      name: 'Kolobok',
+    const loadFilmAction = {
+      type: ActionType.LOAD_FILM,
+      payload: film,
     };
 
-    expect(filmsData(state, loadFilm(film)))
+    expect(filmsData(state, loadFilmAction))
       .toEqual({
         ...state,
         film: {data: film, loaded: true},
@@ -67,18 +141,12 @@ describe('Reducer: filmsData', () => {
   });
 
   it('loadSimilar', () => {
-    const films = [
-      {
-        id: 1,
-        name: 'Kolobok',
-      },
-      {
-        id: 2,
-        name: 'Nu pogodi',
-      },
-    ];
+    const loadSimilarAction = {
+      type: ActionType.LOAD_SIMILAR,
+      payload: films,
+    };
 
-    expect(filmsData(state, loadSimilar(films)))
+    expect(filmsData(state, loadSimilarAction))
       .toEqual({
         ...state,
         similar: films,
@@ -86,18 +154,12 @@ describe('Reducer: filmsData', () => {
   });
 
   it('loadFavorites', () => {
-    const films = [
-      {
-        id: 1,
-        name: 'Kolobok',
-      },
-      {
-        id: 2,
-        name: 'Nu pogodi',
-      },
-    ];
+    const loadFavoritesAction = {
+      type: ActionType.LOAD_FAVORITES,
+      payload: films,
+    };
 
-    expect(filmsData(state, loadFavorites(films)))
+    expect(filmsData(state, loadFavoritesAction))
       .toEqual({
         ...state,
         favorites: {data: films, loaded: true},
@@ -105,21 +167,56 @@ describe('Reducer: filmsData', () => {
   });
 
   it('loadComments', () => {
-    const comments = [
-      {
-        id: 1,
-        name: 'Kolobok',
-      },
-      {
-        id: 2,
-        name: 'Nu pogodi',
-      },
-    ];
+    const loadCommentsAction = {
+      type: ActionType.LOAD_COMMENTS,
+      payload: comments,
+    };
 
-    expect(filmsData(state, loadComments(comments)))
+    expect(filmsData(state, loadCommentsAction))
       .toEqual({
         ...state,
         comments,
+      });
+  });
+
+  it('loadPlayer', () => {
+    const playerState = {
+      promo: {data: {}, loaded: false},
+      film: {data: {}, loaded: false},
+      favorites: {data: [], loaded: false},
+      similar: [],
+      comments: [],
+      player: {},
+      films: {data: films, loaded: true},
+    };
+
+    const loadPlayerAction = {
+      type: ActionType.LOAD_PLAYER,
+      payload: 2,
+    };
+
+    expect(filmsData(playerState, loadPlayerAction))
+      .toEqual({
+        ...playerState,
+        player: {
+          id: 2,
+          name: 'The Grand Budapest Hotel',
+          posteriImage: 'img/the-grand-budapest-hotel-poster.jpg',
+          previewImage: 'img/the-grand-budapest-hotel.jpg',
+          backgroundImage: 'img/the-grand-budapest-hotel-bg.jpg',
+          backgroundColor: '#ffffff',
+          videoLink: 'https://some-link',
+          previewVideoLink: 'https://some-link',
+          description: 'In the 1930s, the Grand Budapest Hotel is a popular European ski resort...',
+          rating: 8.9,
+          scoresCount: 240,
+          director: 'Wes Andreson',
+          starring: ['Bill Murray', 'Edward Norton', 'Jude Law', 'Willem Dafoe', 'Saoirse Ronan'],
+          runTime: 99,
+          genre: 'Comedy',
+          released: 2014,
+          isFavorite: false,
+        },
       });
   });
 });
