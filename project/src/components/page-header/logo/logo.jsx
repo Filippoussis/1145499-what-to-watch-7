934644b73a8ab754fca7d0ce5de1e553c3dfe6ab
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-07-23 08:45:19 +0500

    рефакт Logo: использовал const AppRoute

--- project/src/components/page-header/logo/logo.jsx
+++ project/src/components/page-header/logo/logo.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
 
+import {AppRoute} from '../../../const';
+
 function Logo({footer}) {
 
   const classNameMod = footer ? 'logo__link--light' : '';
 
   return (
     <div className="logo">
-      <Link to="/" className={`logo__link ${classNameMod}`}>
+      <Link to={AppRoute.ROOT} className={`logo__link ${classNameMod}`}>
         <span className="logo__letter logo__letter--1">W</span>
         <span className="logo__letter logo__letter--2">T</span>
         <span className="logo__letter logo__letter--3">W</span>
