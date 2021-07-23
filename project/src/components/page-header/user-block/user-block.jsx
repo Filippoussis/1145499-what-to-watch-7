a542bb1a2f4262b5ea472cbfa1d1e283452dbf68
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-07-23 08:42:39 +0500

    рефакт user-block: select -> useDispatch, useSelector

--- project/src/components/page-header/user-block/user-block.jsx
+++ project/src/components/page-header/user-block/user-block.jsx
@@ -1,29 +1,32 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import PropTypes from 'prop-types';
-import {connect} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import {logout} from '../../../store/actions/api-actions';
 import {getAuthorizationStatus} from '../../../store/reducers/user/selectors';
 
-import {AuthorizationStatus} from '../../../const';
+import {AuthorizationStatus, AppRoute} from '../../../const';
 
-function UserBlock(props) {
+function UserBlock() {
+  const dispatch = useDispatch();
+  const authorizationStatus = useSelector(getAuthorizationStatus);
 
-  const notAuth = <div><Link to="/login" className="user-block__link">Sign in</Link></div>;
+  const handleClick = () => dispatch(logout());
+
+  const notAuth = <div><Link to={AppRoute.LOGIN} className="user-block__link">Sign in</Link></div>;
   const auth = (
     <>
       <div className="user-block__item">
         <div className="user-block__avatar">
-          <Link to="/mylist"><img src="img/avatar.jpg" alt="User avatar" width="63" height="63" /></Link>
+          <Link to={AppRoute.FAVORITES}><img src="img/avatar.jpg" alt="User avatar" width="63" height="63" /></Link>
         </div>
       </div>
       <div className="user-block__item">
-        <span className="user-block__link" onClick={props.logout}>Sign out</span>
+        <span className="user-block__link" onClick={handleClick}>Sign out</span>
       </div>
     </>
   );
 
-  const content = props.authorizationStatus !== AuthorizationStatus.AUTH ? notAuth : auth;
+  const content = authorizationStatus !== AuthorizationStatus.AUTH ? notAuth : auth;
 
   return (
     <div className="user-block">
@@ -32,17 +35,4 @@ function UserBlock(props) {
   );
 }
 
-UserBlock.propTypes = {
-  logout: PropTypes.func.isRequired,
-  authorizationStatus: PropTypes.string.isRequired,
-};
-
-const mapStateToProps = (state) => ({
-  authorizationStatus: getAuthorizationStatus(state),
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  logout: () => dispatch(logout()),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(UserBlock);
+export default UserBlock;
