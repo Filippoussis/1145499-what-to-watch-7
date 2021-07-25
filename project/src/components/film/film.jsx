21c82c1564ce3274fd074e6419cda8b2b0fc22d2
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-07-26 00:04:31 +0500

    рефакт Film -> все на хуки + вынес FilmCardButtons

--- project/src/components/film/film.jsx
+++ project/src/components/film/film.jsx
@@ -1,48 +1,36 @@
 import React, {useEffect, useCallback} from 'react';
-import {Link, useHistory, useParams} from 'react-router-dom';
-import PropTypes from 'prop-types';
-import {connect} from 'react-redux';
+import {useParams} from 'react-router-dom';
+import {useDispatch, useSelector} from 'react-redux';
 
-import {fetchFilm, fetchSimilar, fetchComments, addFilmFavorite} from '../../store/actions/api-actions';
+import {fetchFilm, fetchSimilar, fetchComments} from '../../store/actions/api-actions';
 import {getFilmData, getLoadedFilmStatus} from '../../store/reducers/films-data/selectors';
-import {getAuthorizationStatus} from '../../store/reducers/user/selectors';
-
-import filmProp, {filmDefault} from '../../props/film';
-import {AuthorizationStatus} from '../../const';
 
 import Logo from '../page-header/logo/logo';
 import UserBlock from '../page-header/user-block/user-block';
+import FilmCardButtons from '../film-card-buttons/film-card-buttons';
 import FilmCardDesc from './film-card-desc/film-card-desc';
 import Similar from './similar/similar';
 import PageFooter from '../page-footer/page-footer';
 import Spinner from '../spinner/spinner';
 
-function Film(props) {
+function Film() {
 
-  const {film, loading, loadFilm, loadSimilar, loadComments, authorizationStatus, setFavorite} = props;
-  const {id, name, posterImage, backgroundImage, genre, released, isFavorite} = film;
+  const dispatch = useDispatch();
 
   const params = useParams();
   const {id: matchId} = params;
 
-  const history = useHistory();
-  const redirectOnPlayer = () => history.push(`/player/${id}`);
-  const redirectOnLogin = () => history.push('/login');
-
-  const handleClickButtonList = () => {
-    if (authorizationStatus === AuthorizationStatus.AUTH) {
-      setFavorite(id, Number(!isFavorite));
-    } else {
-      redirectOnLogin();
-    }
-  };
-
   const request = useCallback(() => {
-    loadFilm(matchId);
-    loadSimilar(matchId);
-    loadComments(matchId);
-  }, [loadFilm, loadSimilar, loadComments, matchId]);
+    dispatch(fetchFilm(matchId));
+    dispatch(fetchSimilar(matchId));
+    dispatch(fetchComments(matchId));
+  }, [dispatch, matchId]);
   useEffect(() => request(), [request]);
+
+  const film = useSelector(getFilmData);
+  const loading = useSelector(getLoadedFilmStatus);
+
+  const {id, name, posterImage, backgroundImage, genre, released, isFavorite} = film;
 
   if (!loading) {
     return <Spinner />;
@@ -71,27 +59,8 @@ function Film(props) {
                 <span className="film-card__year">{released}</span>
               </p>
 
-              <div className="film-card__buttons">
-                <button className="btn btn--play film-card__button" type="button" onClick={redirectOnPlayer}>
-                  <svg viewBox="0 0 19 19" width="19" height="19">
-                    <use xlinkHref="#play-s"></use>
-                  </svg>
-                  <span>Play</span>
-                </button>
-                <button className="btn btn--list film-card__button" type="button" onClick={handleClickButtonList}>
-                  {!isFavorite || authorizationStatus !== AuthorizationStatus.AUTH ? (
-                    <svg viewBox="0 0 19 20" width="19" height="20">
-                      <use xlinkHref="#add"></use>
-                    </svg>
-                  ) : (
-                    <svg viewBox="0 0 18 14" width="18" height="14">
-                      <use xlinkHref="#in-list"></use>
-                    </svg>
-                  )}
-                  <span>My list</span>
-                </button>
-                {authorizationStatus === AuthorizationStatus.AUTH ? <Link to={`/films/${id}/review`} className="btn film-card__button">Add review</Link> : null}
-              </div>
+              <FilmCardButtons id={id} isFavorite={isFavorite} isPromo={false} />
+
             </div>
           </div>
         </div>
@@ -102,7 +71,7 @@ function Film(props) {
               <img src={posterImage} alt={`${name} poster`} width="218" height="327" />
             </div>
 
-            {loading ? <FilmCardDesc film={props.film} /> : null}
+            {loading ? <FilmCardDesc film={film} /> : null}
 
           </div>
         </div>
@@ -115,31 +84,4 @@ function Film(props) {
   );
 }
 
-Film.defaultProps = {
-  film: filmDefault,
-};
-
-Film.propTypes = {
-  film: filmProp,
-  loading: PropTypes.bool.isRequired,
-  loadFilm: PropTypes.func.isRequired,
-  loadSimilar: PropTypes.func.isRequired,
-  loadComments: PropTypes.func.isRequired,
-  authorizationStatus: PropTypes.string.isRequired,
-  setFavorite: PropTypes.func.isRequired,
-};
-
-const mapStateToProps = (state) => ({
-  film: getFilmData(state),
-  loading: getLoadedFilmStatus(state),
-  authorizationStatus: getAuthorizationStatus(state),
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  loadFilm: (filmId) => dispatch(fetchFilm(filmId)),
-  loadSimilar: (filmId) => dispatch(fetchSimilar(filmId)),
-  loadComments: (filmId) => dispatch(fetchComments(filmId)),
-  setFavorite: (filmId, status) => dispatch(addFilmFavorite(filmId, status)),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Film);
+export default Film;
