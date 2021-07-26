5e951b7d33390c1a49a7be5f88a57d8a8085332e
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-07-26 23:17:01 +0500

    добавил тестов для api-actions

--- project/src/store/actions/api-actions.test.js
+++ project/src/store/actions/api-actions.test.js
@@ -3,7 +3,9 @@ import {createAPI} from '../../services/api';
 import {ActionType} from './actions';
 import {AuthorizationStatus} from '../../const';
 import {adaptFilmDataToClient} from '../../utils/adapter';
-import {fetchPromo, fetchFilms, fetchFavorites, checkAuth, login, logout
+import {
+  fetchPromo, fetchFilms, fetchFavorites, checkAuth, login, logout,
+  fetchFilm, fetchSimilar, fetchComments, fetchComment, addFavorite
 } from './api-actions';
 
 let api = null;
@@ -133,6 +135,152 @@ describe('Async operations', () => {
         expect(dispatch).toHaveBeenNthCalledWith(1, {
           type: ActionType.LOAD_FAVORITES,
           payload: [{fake: true}].map(adaptFilmDataToClient),
+        });
+      });
+  });
+
+  it('should make a correct API call to GET /film', () => {
+    const apiMock = new MockAdapter(api);
+    const dispatch = jest.fn();
+    const fakeFilmId = 5;
+    const filmLoader = fetchFilm(fakeFilmId);
+
+    apiMock
+      .onGet(`/films/${fakeFilmId}`)
+      .reply(200, [{fake: true}]);
+
+    return filmLoader(dispatch, () => {}, api)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledTimes(1);
+        expect(dispatch).toHaveBeenNthCalledWith(1, {
+          type: ActionType.LOAD_FILM,
+          payload: adaptFilmDataToClient([{fake: true}]),
+        });
+      });
+  });
+
+  it('should make a correct API call to GET /similar', () => {
+    const apiMock = new MockAdapter(api);
+    const dispatch = jest.fn();
+    const fakeFilmId = 3;
+    const similarLoader = fetchSimilar(fakeFilmId);
+
+    apiMock
+      .onGet(`/films/${fakeFilmId}/similar`)
+      .reply(200, [{fake: true}]);
+
+    return similarLoader(dispatch, () => {}, api)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledTimes(1);
+        expect(dispatch).toHaveBeenNthCalledWith(1, {
+          type: ActionType.LOAD_SIMILAR,
+          payload: [{fake: true}].map(adaptFilmDataToClient),
+        });
+      });
+  });
+
+  it('should make a correct API call to GET /comments', () => {
+    const apiMock = new MockAdapter(api);
+    const dispatch = jest.fn();
+    const fakeFilmId = 4;
+    const commentsLoader = fetchComments(fakeFilmId);
+
+    apiMock
+      .onGet(`/comments/${fakeFilmId}`)
+      .reply(200, [{fake: true}]);
+
+    return commentsLoader(dispatch, () => {}, api)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledTimes(1);
+        expect(dispatch).toHaveBeenNthCalledWith(1, {
+          type: ActionType.LOAD_COMMENTS,
+          payload: [{fake: true}],
+        });
+      });
+  });
+
+  it('should make a correct API call to POST /comment', () => {
+    const apiMock = new MockAdapter(api);
+    const dispatch = jest.fn();
+    const fakeFilmId = 2;
+    const fakeComment = {id: 3, comment: 'Super film!'};
+    const commentLoader = fetchComment(fakeFilmId, fakeComment);
+
+    apiMock
+      .onPost(`/comments/${fakeFilmId}`)
+      .reply(200, [{fake: true}]);
+
+    return commentLoader(dispatch, () => {}, api)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledTimes(1);
+        expect(dispatch).toHaveBeenNthCalledWith(1, {
+          type: ActionType.REDIRECT_TO_ROUTE,
+          payload: `/films/${fakeFilmId}`,
+        });
+      });
+  });
+
+  it('error to POST /comment', () => {
+    const apiMock = new MockAdapter(api);
+    const dispatch = jest.fn();
+    const fakeFilmId = 2;
+    const fakeComment = {id: 3, comment: 'Super film!'};
+    const commentLoader = fetchComment(fakeFilmId, fakeComment);
+
+    apiMock
+      .onPost(`/comments/${fakeFilmId}`)
+      .reply(400, [{fake: true}]);
+
+    return commentLoader(dispatch, () => {}, api)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledTimes(1);
+        expect(dispatch).toHaveBeenNthCalledWith(1, {
+          type: ActionType.SET_UNEXPECTED_ERROR,
+          payload: true,
+        });
+      });
+  });
+
+  it('should make a correct API call to POST /favorite', () => {
+    const apiMock = new MockAdapter(api);
+    const dispatch = jest.fn();
+    const fakeFilmId = 7;
+    const status = 1;
+    const isPromo = true;
+    const addFavoriteLoader = addFavorite(fakeFilmId, status, isPromo);
+
+    apiMock
+      .onPost(`/favorite/${fakeFilmId}/${status}`)
+      .reply(200, [{fake: true}]);
+
+    return addFavoriteLoader(dispatch, () => {}, api)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledTimes(1);
+        expect(dispatch).toHaveBeenNthCalledWith(1, {
+          type: ActionType.LOAD_PROMO,
+          payload: adaptFilmDataToClient([{fake: true}]),
+        });
+      });
+  });
+
+  it('error to POST /favorite', () => {
+    const apiMock = new MockAdapter(api);
+    const dispatch = jest.fn();
+    const fakeFilmId = 7;
+    const status = 0;
+    const isPromo = false;
+    const addFavoriteLoader = addFavorite(fakeFilmId, status, isPromo);
+
+    apiMock
+      .onPost(`/favorite/${fakeFilmId}/${status}`)
+      .reply(401, [{fake: true}]);
+
+    return addFavoriteLoader(dispatch, () => {}, api)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledTimes(1);
+        expect(dispatch).toHaveBeenNthCalledWith(1, {
+          type: ActionType.REDIRECT_TO_ROUTE,
+          payload: '/login',
         });
       });
   });
