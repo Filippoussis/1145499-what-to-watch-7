d508ab260e1ef569503fa5108e827f87cd81dd8b
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-07-26 16:31:47 +0500

    перенес логику фильтрации по жанрам в селекторы

--- project/src/store/reducers/events/selectors.js
+++ project/src/store/reducers/events/selectors.js
@@ -1,4 +1,19 @@
 import {NameSpace} from '../root-reducer';
 
+import getGenresData from '../../../utils/genres';
+import {DEFAULT_GENRE} from '../../../const';
+
 export const getCurrentGenre = (state) => state[NameSpace.EVENT].currentGenre;
 export const getDisplayedFilmsCount = (state) => state[NameSpace.EVENT].displayedFilmsCount;
+
+
+export const getGenres = (state) => {
+  const films = state[NameSpace.DATA].films.data;
+  return getGenresData(films);
+};
+
+export const getFiltredFilms = (state) => {
+  const films = state[NameSpace.DATA].films.data;
+  const currentGenre = state[NameSpace.EVENT].currentGenre;
+  return currentGenre !== DEFAULT_GENRE ? films.filter((film) => film.genre === currentGenre) : films;
+};
