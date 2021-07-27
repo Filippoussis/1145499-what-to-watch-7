5b219639943bc8ed929a3e1b5cc69323ee62e0d3
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-07-28 00:26:12 +0500

    добавил тесты для ReviewText

---
+++ project/src/components/add-review/review-form/review-text/review-text.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {render, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import ReviewText from './review-text';
+
+describe('Component: ReviewText', () => {
+  it('should render correctly', () => {
+    render(<ReviewText isDisabledSubmit={false} />);
+
+    expect(screen.getByPlaceholderText('Review text')).toBeInTheDocument();
+    userEvent.type(screen.getByPlaceholderText('Review text'), 'Very good film!');
+    expect(screen.getByDisplayValue('Very good film!')).toBeInTheDocument();
+  });
+});
