8e84366bcef6ee8a397bba4022dfecc1ff609c55
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-07-27 20:41:32 +0500

    рефакт show-more-button.test

--- project/src/components/main/catalog/show-more-button/show-more-button.test.js
+++ project/src/components/main/catalog/show-more-button/show-more-button.test.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import {Provider} from 'react-redux';
 import configureStore from 'redux-mock-store';
-import {render} from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 
 import ShowMoreButton from './show-more-button';
 
 const mockStore = configureStore({});
+let store;
+let fakeApp;
 
-test('ShowMoreButton', () => {
-  const {getByText, getByRole} = render(
-    <Provider store={mockStore({})}>
-      <ShowMoreButton />
-    </Provider>,
-  );
-  const labelElement = getByText('Show more');
-  const roleElement = getByRole('button');
+describe('Component: ShowMoreButton', () => {
+  beforeAll(() => {
+    store = mockStore({});
 
-  expect(labelElement).toBeInTheDocument();
-  expect(roleElement).toBeInTheDocument();
+    fakeApp = (
+      <Provider store={store}>
+        <ShowMoreButton />
+      </Provider>
+    );
+  });
+
+  it('should render correctly', () => {
+    render(fakeApp);
+
+    expect(screen.getByText('Show more')).toBeInTheDocument();
+    expect(screen.getByRole('button')).toBeInTheDocument();
+  });
 });
