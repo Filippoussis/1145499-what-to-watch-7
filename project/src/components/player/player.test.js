06836ff988bc809d8d5b249341f5a529134a4100
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-07-28 19:30:24 +0500

    добавил тесты для Player

---
+++ project/src/components/player/player.test.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {render, screen} from '@testing-library/react';
+import {Provider} from 'react-redux';
+import {Router} from 'react-router-dom';
+import {createMemoryHistory} from 'history';
+import configureStore from 'redux-mock-store';
+import * as Redux from 'react-redux';
+
+import Player from './player';
+
+let store = null;
+let history = null;
+
+describe('Component: Player', () => {
+  beforeAll(() => {
+    history = createMemoryHistory();
+
+    const mockStore = configureStore({});
+    store = mockStore({
+      DATA: {film: {data: {}, loaded: true}},
+    });
+  });
+
+  it('should render correctly', () => {
+    const dispatch = jest.fn();
+    const useDispatch = jest.spyOn(Redux, 'useDispatch');
+    useDispatch.mockReturnValue(dispatch);
+
+    const controlsCount = 3;
+
+    render(
+      <Provider store={store}>
+        <Router history={history}>
+          <Player />
+        </Router>
+      </Provider>,
+    );
+
+    expect(screen.getAllByRole('button').length).toBe(controlsCount);
+  });
+});
