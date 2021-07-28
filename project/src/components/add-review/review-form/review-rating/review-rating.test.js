b7256e14de4edbe2a574204ee3af0a15fbec06cc
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-07-28 08:49:53 +0500

    добавил тесты для ReviewRating

---
+++ project/src/components/add-review/review-form/review-rating/review-rating.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {render, screen} from '@testing-library/react';
+
+import ReviewRating from './review-rating';
+
+describe('Component: ReviewRating', () => {
+  it('should render correctly', () => {
+
+    render(<ReviewRating currentRating={'5'} />);
+
+    expect(screen.getAllByRole('radio').length).toBe(10);
+  });
+});
