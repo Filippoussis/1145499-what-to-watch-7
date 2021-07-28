2aff2386eae84928736a383a059dd1caea584d3f
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-07-28 20:23:17 +0500

    добавил тесты для FilmCardNav

---
+++ project/src/components/film/film-card-desc/film-card-nav/film-card-nav.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {render, screen} from '@testing-library/react';
+
+import FilmCardNav from './film-card-nav';
+
+describe('Component: FilmCardNav', () => {
+  it('should render correctly', () => {
+    const fakeActiveItem = 'Details';
+    const fakeSelectItem = jest.fn();
+    const filmNavItemCount = 3;
+
+    const {container} =render(<FilmCardNav activeItem={fakeActiveItem} selectItem={fakeSelectItem} />);
+
+    expect(container.querySelectorAll('.film-nav__item').length).toBe(filmNavItemCount);
+    expect(screen.getByText('Overview')).toBeInTheDocument();
+    expect(screen.getByText('Details')).toBeInTheDocument();
+    expect(screen.getByText('Reviews')).toBeInTheDocument();
+  });
+});
