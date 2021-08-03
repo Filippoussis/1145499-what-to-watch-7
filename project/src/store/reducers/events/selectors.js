520e56dee4e7dd8a7d657b6ef085797391933923
Andrey Filippov <androsphilippos@gmail.com>
2021-08-03 09:56:13 +0500

    использовал reselect

--- project/src/store/reducers/events/selectors.js
+++ project/src/store/reducers/events/selectors.js
@@ -1,19 +1,21 @@
+import {createSelector} from 'reselect';
 import {NameSpace} from '../root-reducer';
 
 import getGenresData from '../../../utils/genres';
 import {DEFAULT_GENRE} from '../../../const';
 
+const getFilms = (state) =>  state[NameSpace.DATA].films.data;
+
 export const getCurrentGenre = (state) => state[NameSpace.EVENT].currentGenre;
 export const getDisplayedFilmsCount = (state) => state[NameSpace.EVENT].displayedFilmsCount;
 
+export const getGenres = createSelector(
+  getFilms,
+  (films) => getGenresData(films),
+);
 
-export const getGenres = (state) => {
-  const films = state[NameSpace.DATA].films.data;
-  return getGenresData(films);
-};
-
-export const getFiltredFilms = (state) => {
-  const films = state[NameSpace.DATA].films.data;
-  const currentGenre = state[NameSpace.EVENT].currentGenre;
-  return currentGenre !== DEFAULT_GENRE ? films.filter((film) => film.genre === currentGenre) : films;
-};
+export const getFiltredFilms = createSelector(
+  getFilms,
+  getCurrentGenre,
+  (films, currentGenre) => currentGenre !== DEFAULT_GENRE ? films.filter((film) => film.genre === currentGenre) : films,
+);
