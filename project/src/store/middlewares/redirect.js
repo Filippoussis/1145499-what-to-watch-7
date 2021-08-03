726e71868cc0fd7140a43eba578a71fa197fb6e0
Andrey Filippov <androsphilippos@gmail.com>
2021-08-03 10:17:26 +0500

    заменил "REDIRECT_TO_ROUTE" -> ActionType.REDIRECT_TO_ROUTE

--- project/src/store/middlewares/redirect.js
+++ project/src/store/middlewares/redirect.js
@@ -1,7 +1,8 @@
 import browserHistory from '../../browser-history';
+import {ActionType} from '../actions/actions';
 
 export const redirect = (_store) => (next) => (action) => {
-  if (action.type === 'REDIRECT_TO_ROUTE') {
+  if (action.type === ActionType.REDIRECT_TO_ROUTE) {
     browserHistory.push(action.payload);
   }
 
